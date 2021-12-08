fee73bad6c98d43582480e8fe853d95ee71234d2
Michael Rausch <me@michaelrausch.nz>
2021-12-09 01:31:20 +1300

    Removed dist/ and dev.db from git repo

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const start_1 = require("./start");
-// logger.setLevel('info')
-(0, start_1.startServer)();
-//# sourceMappingURL=index.js.map
